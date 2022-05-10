4bf1acc6d88a74a74e9256361e8d6e5b0810607c
Kevin Bookwalter <kbookwal@udel.edu>
2022-05-10 00:12:52 -0400

    New and bigger dataset of 333,333 most common English words
    Up to about 42% faster than static keyboard

--- train_trees.py
+++ train_trees.py
@@ -2,12 +2,22 @@ from tree import NgramTree
 from nltk.corpus import words as WORDS
 from nltk.util import ngrams
 import pickle
+import csv
 
-def train_character_tree():
+def train_character_tree_nltk_words():
     t = NgramTree('<S>')
     all_words = [word.upper() for word in WORDS.words()]
     t.generate_ngrams_from_list(all_words)
     pickle.dump(t, open("character_ngram_tree.p", "wb"))
 
+def train_character_tree_frequent_words():
+    t = NgramTree('<S>')
+    file = open('unigram_freq.csv')
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    words = [row[0].upper() for row in csvreader]
+    t.generate_ngrams_from_list(words)
+    pickle.dump(t, open("character_ngram_tree.p", "wb"))
 
-train_character_tree()
+
+train_character_tree_frequent_words()
--- tree_tests.py
+++ tree_tests.py
@@ -1,7 +1,11 @@
 from tree import NgramTree
 from nltk.corpus import words as WORDS
+from nltk.corpus import brown
+from nltk.corpus import wordnet2021
+from nltk.corpus import switchboard
 from nltk.util import ngrams
 import pickle
+import sys
 
 def predictions_test():
     words = ['ABCD', 'ABCE', 'BCD', 'BCD', 'BCF', 'BCG', 'CH']
@@ -19,4 +23,20 @@ def ngram_length_test():
     t = pickle.load(open("character_ngram_tree.p", "rb"))
     print(t.get_ngram_count("AARDVARK"))
 
-ngram_length_test()
+def switch_board_test():
+    convs = switchboard.words()
+    original_stdout = sys.stdout
+    with open("switchboard.txt", "w") as f:
+        sys.stdout = f
+        for word in convs:
+            print(word)
+
+    sys.stdout = original_stdout
+
+def tmp_test():
+    words = wordnet2021.words()
+    print(len(words))
+
+
+
+tmp_test()
