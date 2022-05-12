f438cf509b87465c9b9565fcd0fd4df72d0fb557
Kevin Bookwalter <kbookwal@udel.edu>
2022-05-12 18:31:14 -0400

    Added test file, made some changes to see overall improvement

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Final Project for CISC 489 - Intro to NLP
 Kevin Bookwalter
+
+https://www.kaggle.com/datasets/rtatman/english-word-frequency?resource=download
--- aac.py
+++ aac.py
@@ -1,14 +1,92 @@
 from predictive_keyboard import PredictiveKeyboard
-from nltk.corpus import gutenberg
+from nltk.corpus import gutenberg, switchboard
 from time import time
 from tree import NgramTree
+import re
+import sys
 
 def main():
 
     kb = PredictiveKeyboard()
 
-    data = prep_data()
+    static_scans = 0
+    dynamic_scans = 0
 
+    original_stdout = sys.stdout
+    with open("results.txt", "w") as f:
+        sys.stdout = f
+        s, d = run_gutenberg(kb)
+        static_scans += s
+        dynamic_scans += d
+
+        s, d = run_switchboard(kb)
+        static_scans += s
+        dynamic_scans += d
+
+        s, d = run_overheard(kb)
+        static_scans += s
+        dynamic_scans += d
+
+        s, d = run_test_file(kb)
+        static_scans += s
+        dynamic_scans += d
+
+        diff = (static_scans - dynamic_scans) / static_scans
+        print("\n==================================================================\n")
+        print("Total Difference (scans): ", diff * 100, "% faster\n")
+    
+    sys.stdout = original_stdout
+
+
+def run_gutenberg(kb):
+    static_scans = 0
+    dynamic_scans = 0
+
+    gutenberg_texts = ["bible-kjv", "carroll-alice", "melville-moby_dick", "austen-sense"]
+
+    for title in gutenberg_texts:
+        data = prep_gutenberg(title)
+        s, d = run_test(kb, data, title)
+        static_scans += s
+        dynamic_scans += d
+
+    return static_scans, dynamic_scans
+
+def run_switchboard(kb):
+    static_scans = 0
+    dynamic_scans = 0
+
+    sb_words = prep_switchboard()
+    s, d = run_test(kb, sb_words, "switchboard")
+
+    static_scans += s
+    dynamic_scans += d
+
+    return static_scans, dynamic_scans
+
+def run_overheard(kb):
+    static_scans = 0
+    dynamic_scans = 0
+
+    overheard = prep_overheard()
+    s, d = run_test(kb, overheard, "overheard")
+
+    static_scans += s
+    dynamic_scans += d
+
+    return static_scans, dynamic_scans
+
+def run_test_file(kb):
+    test_file = open("testfile.txt", "r", encoding="utf-8")
+    text = test_file.read()
+    s,d = run_test(kb, text, "test file")
+    return s,d
+
+    
+
+def run_test(kb, data, title):
+    print("\n==================================================================\n")
+    print("Typing " + title)
     time_start = time()
     static_scans = kb.type_sentence_static(data.upper())
     time_end = time()
@@ -27,11 +105,32 @@ def main():
     diff = (static_scans - dynamic_scans) / static_scans
     print("Difference (scans): ", diff * 100, "% faster\n")
 
-def prep_data():
-    text = gutenberg.raw("carroll-alice.txt")
+    return static_scans, dynamic_scans
+
+def prep_gutenberg(title):
+    text = gutenberg.raw(title + '.txt')
     text.replace("  ", " ")
     text.replace("\n", " ")
     return text.upper()
+
+def prep_switchboard():
+    words = switchboard.words()
+    words_str = ""
+    for word in words:
+        words_str += word + " "
+    return words_str
+
+def prep_overheard():
+    file = open('overheard.txt', 'r', encoding='utf-8')
+    lines_raw = file.readlines()
+    lines = [re.sub(r".*: ", r"", line) for line in lines_raw]
+
+    str_text = ""
+    for line in lines:
+        str_text += re.sub(r'\n', '', line) + " "
+
+    return str_text
+
 
 if __name__ == '__main__':
     main()
