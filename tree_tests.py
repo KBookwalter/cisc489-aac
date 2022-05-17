46ba53e1846d509d706f982bb36b72c492272194
Kevin Bookwalter <kbookwal@udel.edu>
2022-05-17 19:57:08 -0400

    Removed unused code

--- tree_tests.py
+++ tree_tests.py
@@ -38,24 +38,3 @@ def switch_board_test():
 def tmp_test():
     words = wordnet2021.words()
     print(len(words))
-
-def word_predictions_test():
-    t = NgramTree('<S>')
-    t.generate_ngrams_from_list(['CAR', 'CAT', 'CAB', 'COLD', 'COLORFUL', 'COD', 'COLOR'])
-    print(t.get_word_predictions('CO'))
-
-def word_predictions_test2():
-    t = pickle.load(open("character_ngram_tree.p", "rb"))
-    print("HORSE" in t.get_word_predictions("HORS"))
-
-def word_list_test():
-    file = open('unigram_freq.csv')
-    csvreader = csv.reader(file)
-    header = next(csvreader)
-    word_ranks = {}
-    for row in csvreader:
-        word_ranks[row[0]] = row[1]
-
-    print(len(word_ranks))
-
-word_predictions_test2()
